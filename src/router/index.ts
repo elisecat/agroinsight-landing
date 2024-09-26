8c7f61139855e1a12f71d94c89b8f50503e8dd1f
Sara Duran <saraelissad@gmail.com>
2024-09-26 00:11:19 -0500

    index ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 
 const routes = [
-  { path: '/', redirect: '/inicio' },
-  { path: '/inicio', name: 'Inicio', component: Home },
+  { path: '/inicio', alias: '/', name: 'Inicio', component: Home }
 ]
 
 const router = createRouter({
